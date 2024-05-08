027b35dc955990f8601f53ed5f6808879864e9ae
alirio <a1105fut@gmail.com>
2024-05-08 18:17:51 -0500

    nav actualizado

---
+++ model/contador/index.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+// Verificar si la sesión no está iniciada
+if (!isset($_SESSION["id_usuario"])) {
+    // Mostrar un alert y redirigir utilizando JavaScript
+    echo '<script>alert("Debes iniciar sesión antes de acceder a la interfaz de administrador.");</script>';
+    echo '<script>window.location.href = "../login.html";</script>';
+    exit();
+}
+
+require_once("../../conexion/conexion.php");
+$db = new Database();
+$con = $db->conectar();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/1057b0ffdd.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php include("nav.php") ?>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
