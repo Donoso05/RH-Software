6b8b314fda344373dd5f8505535991adecf7e4fd
kenner rojas <kenner181@gmail.com>
2024-05-08 10:12:54 -0500

    model empleado

---
+++ model/empleado/nav.php
@@ -0,0 +1,18 @@
+
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">RECURSOS HUMANOS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+        <a class="nav-link" href="usuario.php">Usuarios</a>
+        <a class="nav-link" href="avatar.php">Roles</a>
+        <a class="nav-link" href="mundos.php">Permisos</a>
+        <a class="nav-link" href="../../controller/logout.php">Cerrar sesiòn</a>
+      </div>
+    </div>
+  </div>
+</nav>
