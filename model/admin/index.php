b0dc83d1434b77b51f4eb09e3b4d9f7df08247c8
Andrea Silva <andreasilva23433@gmail.com>
2024-05-22 06:05:34 -0500

    Envio de usuario y correo al empleado, funcional

--- model/admin/index.php
+++ model/admin/index.php
@@ -47,7 +47,6 @@ if ($result) {
 $stmt->closeCursor();
 $con = null;
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -57,6 +56,7 @@ $con = null;
     <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/1057b0ffdd.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/nav.css">
 </head>
 <body>
     <?php include("nav.php") ?>
@@ -67,14 +67,21 @@ $con = null;
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-4">
-                            <img src="<?php echo !empty($foto) ? $foto : 'img/user.webp'; ?>" class="img-fluid rounded" alt="Foto de perfil">
-                            <form action="upload.php" method="post" enctype="multipart/form-data" style="margin-top: 20px;">
-                                <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*">
-                                <br>
-                                <br>
-                                
-                                <input type="submit" class="btn btn-primary" value="Actualizar">
-                            </form>
+                            <div class="profile-container">
+                                <div class="profile-img-wrapper">
+                                    <img src="<?php echo !empty($foto) ? $foto : 'img/user.webp'; ?>" class="img-fluid profile-img" alt="Foto de perfil">
+                                </div>
+                                <form action="upload.php" method="post" enctype="multipart/form-data" style="margin-top: 20px;">
+                                    <div class="file-input-wrapper">
+                                        <label class="file-input">
+                                            <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*">
+                                            Actualizar Foto
+                                        </label>
+                                    </div>
+                                    <br>
+                                    <input type="submit" class="btn btn-primary" value="Actualizar">
+                                </form>
+                            </div>
                         </div>
                         <div class="col-md-8">
                             <h2><?php echo $nombre; ?></h2>
@@ -91,4 +98,3 @@ $con = null;
     </div>
 </body>
 </html>
-
